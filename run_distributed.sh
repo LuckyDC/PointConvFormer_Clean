34110962ea4921df59b80ab6d6a71bacb4646837
dylanwu711 <dylanwu711@outlook.com>
2022-07-04 17:02:21 -0700

    update train command

--- run_distributed.sh
+++ run_distributed.sh
@@ -1,4 +1,2 @@
-# python -m torch.distributed.launch --nproc_per_node 8 train_ScanNet_guided_2cm_DDPV2.py # --config ./configWenxuanGuide2cmDDPL5.yaml
-# python -m torch.distributed.launch --nproc_per_node 8 train_ScanNet_guided_2cm_DDP_WarmUP_FT.py --config ./configWenxuanGuide2cmDDPL5WarmUP.yaml
-python -m torch.distributed.launch --nproc_per_node 8 train_ScanNet_guided_2cm_DDP_WarmUP.py --config ./configWenxuanGuide2cmDDPL5WarmUP.yaml
-# python -m torch.distributed.launch --nproc_per_node 8 train_ScanNet_guided_2cm_DDP_WarmUP.py --config ./configWenxuanGuide2cmDDPL6WarmUP.yaml
+
+python -m torch.distributed.launch --nproc_per_node 2 train_ScanNet_DDP_WarmUP.py --config ./configWenxuanPCFDDPL5WarmUP.yaml
